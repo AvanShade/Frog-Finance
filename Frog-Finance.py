2401193c153f13ed8ba3b1855721f85f518d7c8b
Seth Motta <112897748+AvanShade@users.noreply.github.com>
2024-10-07 19:54:53 -0600

    Give main script a main function and use MainWindow object

--- Frog-Finance.py
+++ Frog-Finance.py
@@ -1,12 +1,15 @@
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget
-
 import sys
 
-app = QApplication([])
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+from ui.main_window import MainWindow
 
-window = QWidget()
-window.setWindowTitle("Frog Finance")
-window.setGeometry(100, 100, 1366, 768) # TODO: Determine final app size
-window.show()
+def main():
+    app = QApplication([]) # Initialize application
 
-sys.exit(app.exec())
+    mainWindow = MainWindow() # Create main window (UI)
+    mainWindow.show() # Show main window
+
+    sys.exit(app.exec()) # Start event loop
+
+if __name__ == '__main__':
+    main()
